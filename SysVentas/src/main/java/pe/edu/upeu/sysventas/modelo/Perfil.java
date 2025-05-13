7a9f82955406ca0ea2ceae2ecc6566d94b971f31
jose2207 <armandomamanip@gmail.com>
2025-05-13 08:41:42 -0500

    13 may

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/modelo/Perfil.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/modelo/Perfil.java
@@ -1,22 +1,20 @@
 package pe.edu.upeu.sysventas.modelo;
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @Data
 @Entity
-@Table(name = "upeu_categoria")
-public class Categoria {
+@Table(name = "upeu_perfil")
+public class Perfil {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_categoria")
-    private Long idCategoria;
-    @Column(name = "nombre", nullable = false, length = 60)
+    @Basic(optional = false)
+    @Column(name = "id_perfil", nullable = false)
+    private Long idPerfil;
+    //@Size(max = 20)
     private String nombre;
+    //@Size(max = 6)
+    private String codigo;
 }
--- SysVentas/src/main/java/pe/edu/upeu/sysventas/modelo/Producto.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/modelo/Producto.java
@@ -5,33 +5,29 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import pe.edu.upeu.sysventas.modelo.Categoria;
-import pe.edu.upeu.sysventas.modelo.Marca;
-import pe.edu.upeu.sysventas.modelo.UnidadMedida;
-
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
-@Table (name = "upeu_producto")
+@Table(name = "upeu_producto")
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_producto")
     private Long idProducto;
-    //@NotNull (message = "El nombre no puede estar vacío")
+    //@NotNull(message = "El nombre no puede estar vacío")
     //@Size(min = 2, max = 120, message = "El nombre debe tener entre 2 y 120 caracteres")
-            @Column(name = "nombre", nullable = false, length = 120)
+    @Column(name = "nombre", nullable = false, length = 120)
     private String nombre;
     //@Positive(message = "El Precio Unitario debe ser positivo")
     @Column(name = "pu", nullable = false)
     private Double pu;
-    //@PositiveOrZero (message = "El Precio Unitario Anterior debe ser positivo o cero")
-            @Column(name = "puold", nullable = false)
-            private Double puOld;
-            //@Positive(message = "La utilidad debe ser positiva")
-            @Column(name = "utilidad", nullable = false)
+    //@PositiveOrZero(message = "El Precio Unitario Anterior debe ser positivo o cero")
+    @Column(name = "puold", nullable = false)
+    private Double puOld;
+    //@Positive(message = "La utilidad debe ser positiva")
+    @Column(name = "utilidad", nullable = false)
     private Double utilidad;
     //@Positive(message = "El Stock debe ser positivo o cero")
     @Column(name = "stock", nullable = false)
@@ -41,17 +37,20 @@ public class Producto {
     private Double stockOld;
     //@NotNull(message = "Categoria no puede estar vacío")
     @ManyToOne
-    @JoinColumn(name = "id_categoria", referencedColumnName = "id_categoria",
-            nullable = false, foreignKey = @ForeignKey(name = "FK_CATEGORIA_PRODUCTO") )
+    @JoinColumn(name = "id_categoria", referencedColumnName =
+            "id_categoria",
+            nullable = false, foreignKey = @ForeignKey(name =
+            "FK_CATEGORIA_PRODUCTO") )
     private Categoria categoria;
     //@NotNull(message = "Marca no puede estar vacío")
     @ManyToOne
     @JoinColumn(name = "id_marca", referencedColumnName = "id_marca",
-            nullable = false, foreignKey = @ForeignKey(name = "FK_MARCA_PRODUCTO"))
+            nullable = false, foreignKey = @ForeignKey(name =
+            "FK_MARCA_PRODUCTO"))
     private Marca marca;
     //@NotNull(message = "Unidad Medida no puede estar vacío")
-    @ManyToOne
-    @JoinColumn(name = "id_unidad", referencedColumnName = "id_unidad",
-            nullable = false, foreignKey = @ForeignKey(name = "FK_UNIDADMEDIDA_PRODUCTO"))
+    @ManyToOne@JoinColumn(name = "id_unidad", referencedColumnName = "id_unidad",
+            nullable = false, foreignKey = @ForeignKey(name =
+            "FK_UNIDADMEDIDA_PRODUCTO"))
     private UnidadMedida unidadMedida;
 }
