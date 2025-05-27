030ccc5681cc54e1dcb752f9bb6bb75247be9f09
dmamanipar <dmamanipar@unsa.edu.pe>
2025-05-27 09:15:12 -0500

    estilos y metodos

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentasApplication.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/SysVentasApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -44,6 +45,7 @@ public class SysVentasApplication extends Application /*implements CommandLineRu
 	@Override
 	public void start(Stage stage) throws Exception {
 		Scene scene = new Scene(parent);
+		scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
 		//scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
 		stage.setScene(scene);
 		stage.setTitle("SysAlmacen Spring Java-FX");
