01ca4f380bef8512718ef1baeeab8415edca6412
dmamanipar <dmamanipar@unsa.edu.pe>
2025-05-27 10:14:42 -0500

    Listar CRUD

--- SysVentas/src/main/java/pe/edu/upeu/sysventas/control/ProductoController.java
+++ SysVentas/src/main/java/pe/edu/upeu/sysventas/control/ProductoController.java
@@ -1,8 +1,11 @@
 package pe.edu.upeu.sysventas.control;
 
+import jakarta.validation.Validation;
 import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -15,12 +18,19 @@ import javafx.util.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import pe.edu.upeu.sysventas.componente.ColumnInfo;
+import pe.edu.upeu.sysventas.componente.ComboBoxAutoComplete;
+import pe.edu.upeu.sysventas.componente.TableViewHelper;
+import pe.edu.upeu.sysventas.componente.Toast;
 import pe.edu.upeu.sysventas.dto.ComboBoxOption;
 import pe.edu.upeu.sysventas.modelo.Producto;
 import pe.edu.upeu.sysventas.servicio.CategoriaService;
 import pe.edu.upeu.sysventas.servicio.MarcaService;
 import pe.edu.upeu.sysventas.servicio.ProductoService;
 import pe.edu.upeu.sysventas.servicio.UnidadMedidaService;
+
+import java.util.LinkedHashMap;
+import java.util.function.Consumer;
 
 @Controller
 public class ProductoController {
@@ -76,8 +86,84 @@ public class ProductoController {
          System.out.println("El título del stage es: " + stage.getTitle());
          });
         */
+
+        cbxMarca.getItems().addAll(ms.listarCombobox());
+        cbxMarca.setOnAction(event -> {
+            ComboBoxOption selectedProduct = cbxMarca.getSelectionModel().getSelectedItem();
+            if (selectedProduct != null) {
+                String selectedId = selectedProduct.getKey(); // Obtener el ID
+                System.out.println("ID del producto seleccionado: " + selectedId);
+            }
+        });
+        new ComboBoxAutoComplete<>(cbxMarca);
+
+        cbxCategoria.getItems().addAll(cs.listarCombobox());
+        cbxCategoria.setOnAction(event -> {
+            ComboBoxOption selectedProduct = cbxCategoria.getSelectionModel().getSelectedItem();
+            if (selectedProduct != null) {
+                String selectedId = selectedProduct.getKey(); // Obtener el ID
+                System.out.println("ID del producto seleccionado: " + selectedId);
+            }
+        });
+        new ComboBoxAutoComplete<>(cbxCategoria);
+
+
+        cbxUnidMedida.getItems().addAll(ums.listarCombobox());
+        cbxUnidMedida.setOnAction(event -> {
+            ComboBoxOption selectedProduct = cbxUnidMedida.getSelectionModel().getSelectedItem();
+            if (selectedProduct != null) {
+                String selectedId = selectedProduct.getKey(); // Obtener el ID
+                System.out.println("ID del producto seleccionado: " + selectedId);
+            }
+        });
+        new ComboBoxAutoComplete<>(cbxUnidMedida);
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+        // Crear instancia de la clase genérica TableViewHelper
+        TableViewHelper<Producto> tableViewHelper = new TableViewHelper<>();
+        LinkedHashMap<String, ColumnInfo> columns = new LinkedHashMap<>();
+        columns.put("ID Pro.", new ColumnInfo("idProducto", 60.0)); // Columna visible "Columna 1" mapea al campo "campo1"
+        columns.put("Nombre Producto", new ColumnInfo("nombre", 200.0)); // Columna visible "Columna 2" mapea al campo "campo2"
+        columns.put("P. Unitario", new ColumnInfo("pu", 150.0)); // Columna visible "Columna 2" mapea al campo "campo2"
+        columns.put("Utilidad", new ColumnInfo("utilidad", 100.0)); // Columna visible "Columna 2" mapea al campo "campo2"
+        columns.put("Marca", new ColumnInfo("marca.nombre", 200.0)); // Columna visible "Columna 2" mapea al campo "campo2"
+        columns.put("Categoria", new ColumnInfo("categoria.nombre", 200.0)); // Columna visible "Columna 2" mapea al campo "campo2"
+// Definir las acciones de actualizar y eliminar
+        Consumer<Producto> updateAction = (Producto producto) -> {
+            System.out.println("Actualizar: " + producto);
+            //editForm(producto);
+        };
+        Consumer<Producto> deleteAction = (Producto producto) -> {System.out.println("Actualizar: " + producto);
+            ps.eliminarRegEntidad(producto.getIdProducto()); /*deletePerson(usuario);*/
+            double with=stage.getWidth()/1.5;
+            double h=stage.getHeight()/2;
+            Toast.showToast(stage, "Se eliminó correctamente!!", 2000, with, h);
+            listar();
+        };
+// Usar el helper para agregar las columnas en el orden correcto
+        tableViewHelper.addColumnsInOrderWithSize(tableView, columns,updateAction, deleteAction );
+// Agregar botones de eliminar y modificar
+        tableView.setTableMenuButtonVisible(true);
+        listar();
+
+
     }
 
+    public void listar(){
+        try {
+            tableView.getItems().clear();
+            listarProducto = FXCollections.observableArrayList(ps.listarEntidad());
+            tableView.getItems().addAll(listarProducto);
+            // Agregar un listener al campo de texto txtFiltroDato para filtrar los productos
+           /* txtFiltroDato.textProperty().addListener((observable, oldValue, newValue) -> {
+                filtrarProductos(newValue);
+            });*/
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 
 }
